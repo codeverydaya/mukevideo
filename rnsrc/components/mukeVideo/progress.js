1b6658f7c4f909605b22d588908d6c68900f2b04
lmy-hub <1013127768@qq.com>
2020-01-16 18:16:13 +0800

    Update progress.js
    优化

--- rnsrc/components/mukeVideo/progress.js
+++ rnsrc/components/mukeVideo/progress.js
@@ -1,6 +1,8 @@
-/*
- * author: lmy
- * */
+/**
+ * @author lmy
+ * @date 2019/08/04 下午12:37
+ * @desc 进度条
+ */
 
 import React, {Component} from 'react';
 import {
@@ -16,7 +18,6 @@ export default class Progress extends Component {
         this.onStart = this.onStart.bind(this);
         this.onMove = this.onMove.bind(this);
         this.onEnd = this.onEnd.bind(this);
-
         this.pageX = 0;//记录触摸按钮的位置
         this.isMove = false;//是否拖动
         //进度条的位置和长度
@@ -24,8 +25,6 @@ export default class Progress extends Component {
             pageX: 0,
             width: 0,
         }
-
-
         this.panResponder = PanResponder.create({
             onStartShouldSetPanResponder: (evt, gestureState) => {
                 return true;
@@ -50,25 +49,25 @@ export default class Progress extends Component {
             onPanResponderTerminate:(evt, gestureState) => {
             },
         });
-
     }
-
     onStart(e, g) {
         //获取 按钮的 x的位置
         this.pageX = e.nativeEvent.pageX;
         this.isMove = true;
     }
 
+    //触摸点移动时回调
     onMove(e, g) {
 
         //获取手指相对屏幕 x的坐标，并设计拖动按钮的位置，拖动按钮不能超出进度条的位置
         this.pageX = e.nativeEvent.pageX;
+
         if (e.nativeEvent.pageX < this.progressLocation.pageX) {
             this.pageX = this.progressLocation.pageX;
         } else if (e.nativeEvent.pageX > (this.progressLocation.pageX + this.progressLocation.width - 10)) {
+            //-10的目的是为了修正触摸点的直径，防止超过100%
             this.pageX = this.progressLocation.pageX + this.progressLocation.width - 10;
         }
-
         this.setState({
             ...this.state
         })
@@ -76,25 +75,28 @@ export default class Progress extends Component {
         this.props.changeCurrentTime((this.pageX - this.progressLocation.pageX) / this.progressLocation.width)
     }
 
+    //触摸结束时回调
     onEnd(e, g) {
         //触摸事件结束后，设置视频的播放进度
         this.props.changeProgress((this.pageX - this.progressLocation.pageX) / this.progressLocation.width)
         this.isMove = false;
     }
 
-
-
     render() {
         return (
-
             //{/*进度条的父元素*/}
             <View onLayout={(event) => {
                 {
-                    //主要拿到这个view的位置和宽度
+                   // let {x, y, width, height} = event.nativeEvent.layout;
+                    //拿到这个view的x位置和宽度
                     NativeModules.UIManager.measure(event.target, (x, y, width, height, pageX, pageY) => {
-                        this.progressLocatio = {
+                        //安卓手机获取的值与ios不一样，特殊处理
+                        if(Util.isPlatform("android")){
+                            x=pageX - Util.getWidth();
+                        }
+                        this.progressLocation = {
                             name: 'progressLocation',
-                            pageX:pageX - Util.getWidth(),
+                            pageX:x,
                             width: width,
                         }
                     })
@@ -119,7 +121,6 @@ export default class Progress extends Component {
             </View>
         );
     }
-
     componentDidMount() {
     };
 }
